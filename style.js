bec65fb8db6404103fc639908a236f96f7844079
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2018-09-07 09:55:23 +0200

    Create style.js

---
+++ style.js
@@ -0,0 +1,84 @@
+(function (blink) {
+	'use strict';
+ 	var defis_demoStyle = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+ 	defis_demoStyle.prototype = {
+		bodyClassName: 'content_type_clase_defis_demo',
+		ckEditorStyles: {
+			name: 'defis_demo',
+			styles: [
+ 				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
+				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
+ 				{ name: 'Lista ordenada1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
+				{ name: 'Lista ordenada2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
+				{ name: 'Lista ordenada3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol3' } },
+				{ name: 'Lista ordenada4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol4' } },
+				{ name: 'Lista ordenada5', element: 'ol', attributes: { 'class': 'bck-ol bck-ol5' } },
+				{ name: 'Lista marron', element: 'ol', attributes: { 'class': 'bck-ol bck-ol6' } },
+				{ name: 'Lista naranja', element: 'ol', attributes: { 'class': 'bck-ol bck-ol7' } },
+				{ name: 'Lista letranaranja', element: 'ol', attributes: { 'class': 'bck-ol bck-ol8' } },
+				{ name: 'Lista azulclaro', element: 'ol', attributes: { 'class': 'bck-ol bck-ol9' } },
+				{ name: 'Lista letrazulclaro', element: 'ol', attributes: { 'class': 'bck-ol bck-ol10' } },
+				
+				{ name: 'Lista Desordenada', element: 'ul', attributes: { 'class': 'bck-ul'} },
+				{ name: 'Lista Desordenada 2', element: 'ul', attributes: { 'class': 'bck-ul-2'} },
+				{ name: 'Lista naranja', element: 'ul', attributes: { 'class': 'bck-ul-3'} },
+				{ name: 'Lista azulclaro', element: 'ul', attributes: { 'class': 'bck-ul-4'} },
+ 				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-2' } },
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-3' } }
+			]
+		},
+ 		init: function () {
+			var parent = blink.theme.styles.basic.prototype;
+			parent.init.call(this);
+			this.addActivityTitle();
+			if(window.esWeb) return;
+			this.removeFinalSlide();
+			this.handleScrollEnd();
+			this.setTooltip();
+			window.editar && this.configEditor();
+ 			if ($('.navbar-bottom').length > 0) {
+ 				$('.navbar-bottom ol').wrapAll('<div id="bottom-navigator"></div>');
+		 		var width = 0;
+		 		$('.navbar-bottom li').each(function(i, elem){ width += $(elem).outerWidth(true); });
+		 		$('.navbar-bottom ol').css('width', width * 1.1);
+		 		var scroll = new IScroll('#bottom-navigator', {
+		 			scrollX: true,
+		 			scrollY: false,
+		 			eventPassthrough: true
+		 		});
+		 		scroll.on('scrollEnd', this.handleScrollEnd);
+		 		this.handleScrollEnd.call(scroll);
+	 		}
+ 		},
+ 		configEditor: function (editor) {
+			editor.dtd.$removeEmpty['span'] = false;
+		},
+ 		addActivityTitle: function () {
+			if (!blink.courseInfo || !blink.courseInfo.unit) return;
+			$('.libro-left').find('.title').html(function () {
+				return $(this).html().trim() + ' > ' + blink.courseInfo.unit;
+			});
+		},
+ 		handleScrollEnd: function () {
+ 			$('#bottom-navigator')
+ 				.removeClass('show_left')
+ 				.removeClass('show_right');
+  			if (this.x < 0) {
+ 				$('#bottom-navigator').addClass('show_left');
+ 			}
+ 			if (this.x > this.maxScrollX) {
+ 				$('#bottom-navigator').addClass('show_right');
+ 			}
+  		},
+ 		setTooltip: function () {},
+ 		getEditorStyles: function () {
+			return this.ckEditorStyles;
+		}
+	};
+ 	defis_demoStyle.prototype = _.extend({}, new blink.theme.styles.basic(), defis_demoStyle.prototype);
+ 	blink.theme.styles.defis_demo = defis_demoStyle;
+ })( blink );
